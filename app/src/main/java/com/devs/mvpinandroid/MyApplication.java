0945170e21dbf0671f6d5999e2289375644005b5
devsideal <devs.ideal@gmail.com>
2016-12-19 13:53:29 +0530

    added memory leak detector

---
+++ app/src/main/java/com/devs/mvpinandroid/MyApplication.java
@@ -0,0 +1,24 @@
+package com.devs.mvpinandroid;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by ${Deven} on 19/12/16.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
